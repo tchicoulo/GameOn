1d9da210edfa5ff95de72419fa64fe9847bcdf7b
Calixte <tchicoulo@hotmail.fr>
2022-01-11 18:13:33 +0100

    form repared, obtaining iputs values

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const validForm = document.querySelector(".valid-form");
+
+const contentModal = document.querySelector(".bground");
+const closeModal = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +24,51 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close Modal Form
+closeModal.addEventListener("click", closeModalForm);
 
+function closeModalForm() {
+  contentModal.style.display = "none";
+}
+
+//Valid & send form
+
+function submitForm(e) {
+  //target Values
+  let firstName = document.getElementById("first").value;
+  let lastName = document.getElementById("last").value;
+  let email = document.getElementById("email").value;
+  let birthDate = document.getElementById("birthdate").value;
+  let quantity = document.getElementById("quantity").value;
+  let radio = document.getElementsByName("location");
+  let acceptTerms = document.getElementById("checkbox1");
+  let radioValue = "";
+
+  //Regex
+  checkName = /[a-zA-Z]{2,}/; // 2 characters or more
+
+  //tests inputs form
+
+  e.preventDefault();
+
+  console.log("prenom :" + firstName);
+  console.log("nom :" + lastName);
+  console.log("mail :" + email);
+  console.log("anniv :" + birthDate);
+  console.log("qté  :" + quantity);
+
+  radio.forEach((elem) => {
+    if (elem.checked) {
+      radioValue = elem.value;
+      console.log(radioValue);
+    }
+  });
+
+  if (acceptTerms.checked) {
+    console.log("Termes acceptés !");
+  } else {
+    console.log("Termes non acceptés");
+  }
+}
+
+validForm.addEventListener("submit", submitForm);
